1729c528dd74293956de046e3bd89773c403cd88
jervisAutomation <jervis.socmedandgames@gmail.com>
2025-09-24 12:36:08 +0800

    reverted changes

--- test-scripts/createPost.test.js
+++ test-scripts/createPost.test.js
@@ -1,9 +1,8 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { BASE_URL } from '../utils/config.js';
-import { readFileSync } from 'fs';
 
-const createPayload = JSON.parse(readFileSync('../payloads/post/createPost.json', 'utf-8'));
+const createPayload = JSON.parse(open('../payloads/post/createPost.json'));
 
 export const options = {
   vus: 5,
--- test-scripts/updatePost.test.js
+++ test-scripts/updatePost.test.js
@@ -1,9 +1,8 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { BASE_URL } from '../utils/config.js';
-import { readFileSync } from 'fs';
 
-const createPayload = JSON.parse(readFileSync('../payloads/post/updatePost.json', 'utf-8'));
+const createPayload = JSON.parse(open('../payloads/post/updatePost.json'));
 
 export const options = {
   vus: 5,
